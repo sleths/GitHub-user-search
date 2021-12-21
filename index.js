329734723d535e4b465d7f102c7913dd8cd9f7b1
sleths <88283604+sleths@users.noreply.github.com>
2021-12-21 23:58:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const avatar = document.querySelector(".avatar");
+const userTitle = document.querySelector(".usertitle");
+const date = document.querySelector(".date");
+const username = document.querySelector(".username");
+const bio = document.querySelector(".bio");
+const repos = document.querySelector(".repos");
+const followers = document.querySelector(".followers");
+const following = document.querySelector(".following");
+const lcation = document.querySelector(".icon-text-location");
+const twitter = document.querySelector(".icon-text-twitter");
+const github = document.querySelector(".icon-text-github");
 const text = "sleths";
 const getUser = async () => {
   const response = await axios.get(`https://api.github.com/users/${text}`);
+  const { login, bio, avatar_url, name } = response.data;
+  username.textContent = login;
+  userTitle.textContent = name;
+  if (response.data.bio) {
+    bio.textContent = bio;
+  }
   console.log(response.data);
+
+  avatar.setAttribute("src", avatar_url);
 };
 
 getUser();
